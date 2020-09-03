42e271221c9f1669da2ae7f3a2c9e403e9255f9c
Darrell Yeo <zeniethlily@gmail.com>
2020-09-03 14:32:18 +0800

    Updates

---
+++ precache-manifest.24d8694178291cfbd168f61fb8f3db8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd3ff6db8b00c82760b7d9744f1de68",
+    "url": "/react_game/index.html"
+  },
+  {
+    "revision": "a8acc75ae4b6415f6e49",
+    "url": "/react_game/static/css/main.52a64a56.chunk.css"
+  },
+  {
+    "revision": "0dbb97d0a072945b3540",
+    "url": "/react_game/static/js/2.9de8a1b8.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/react_game/static/js/2.9de8a1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8acc75ae4b6415f6e49",
+    "url": "/react_game/static/js/main.c3df979c.chunk.js"
+  },
+  {
+    "revision": "8eaf9bceb1b36e4762f5",
+    "url": "/react_game/static/js/runtime-main.ea56facb.js"
+  }
+]);
